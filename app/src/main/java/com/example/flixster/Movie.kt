1b28171619a6580150d7a8698818476dfd52e72f
EmmanuIkeh <tochukwuikye@gmail.com>
2022-03-05 02:14:08 +0000

    updated commit

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,14 +1,23 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 import org.json.JSONArray
 
+@Parcelize
 data class Movie (
+    val backdropPath: String,
     val movieId: Int,
+    val voteAverage: Double,
     private val posterPath: String,
     val title: String,
     val overview: String,
-) {
+) : Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+    @IgnoredOnParcel
+    val backdropImageUrl = "https://image.tmdb.org/t/p/w342/$backdropPath"
     companion object {
         fun fromJsonArray(movieJsonArray: JSONArray): List<Movie> {
             val movies = mutableListOf<Movie>()
@@ -16,7 +25,9 @@ data class Movie (
                 val movieJson = movieJsonArray.getJSONObject(i)
                 movies.add(
                     Movie(
+                        movieJson.getString("backdrop_path"),
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview")
